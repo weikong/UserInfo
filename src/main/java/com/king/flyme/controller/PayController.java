4093933657253efd07452633f96df54b39bcbe5d
平行线 <weikong555@gmail.com>
2017-08-28 14:28:10 +0800

    1、订单支付

--- src/main/java/com/king/flyme/controller/PayController.java
+++ src/main/java/com/king/flyme/controller/PayController.java
@@ -1,6 +1,8 @@
 package com.king.flyme.controller;
 
 import com.king.flyme.bean.OrderItem;
+import com.king.flyme.bean.OrderItemExample;
+import com.king.flyme.bean.Orders;
 import com.king.flyme.dao.OrderItemMapper;
 import com.king.flyme.dao.OrdersMapper;
 import org.apache.commons.collections4.MapUtils;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -21,11 +24,13 @@ public class PayController extends AbsController {
     private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     /**
-     * 加入订单：1
-     * 取消订单：2
-     * 取消支付：4
-     * 支付成功：8
-     * 支付异常：9
+     * 初始化：0；
+     * 待付款：1；
+     * 待发货：2；
+     * 待收货：3；
+     * 取消订单：-1；
+     * 支付失敗：-9；
+     * 已完成：9
      */
 
     @Autowired
@@ -43,14 +48,25 @@ public class PayController extends AbsController {
             int account_id = MapUtils.getInteger(param, "account_id", -1);
             if (account_id == -1)
                 throw new RuntimeException("该账号不存在");
-            //order_item id
+            //orders id
             int id = MapUtils.getInteger(param, "id", -1);
             if (id == -1)
                 throw new RuntimeException("订单不存在");
-            OrderItem orderItem = orderItemMapper.selectByPrimaryKey(id);
-            if (orderItem != null) {
-                orderItem.setState(1);
-                int update = orderItemMapper.updateByPrimaryKeySelective(orderItem);
+            float price = MapUtils.getFloat(param, "price", 0f);
+            Orders orders = ordersMapper.selectByPrimaryKey(id);
+            if (orders != null) {
+                OrderItemExample example = new OrderItemExample();
+                example.createCriteria().andAccountIdEqualTo(account_id).andOrderIdEqualTo(orders.getOrderId());
+                List<OrderItem> list = orderItemMapper.selectByExample(example);
+                float price2 = 0f;
+                for (OrderItem orderItem : list){
+                    price2 += orderItem.getPrice()*orderItem.getCount();
+                }
+                if (price != price2)
+                    throw new RuntimeException("请确认订单金额是否出错");
+                // TODO: 2017/8/28 订单支付
+                orders.setStatus(2);
+                int update = ordersMapper.updateByPrimaryKeySelective(orders);
                 if (update != 1)
                     throw new RuntimeException("订单操作失败");
             }
@@ -75,17 +91,6 @@ public class PayController extends AbsController {
             int account_id = MapUtils.getInteger(param, "account_id", -1);
             if (account_id == -1)
                 throw new RuntimeException("该账号不存在");
-            //order_item id
-            int id = MapUtils.getInteger(param, "id", -1);
-            if (id == -1)
-                throw new RuntimeException("订单不存在");
-            OrderItem orderItem = orderItemMapper.selectByPrimaryKey(id);
-            if (orderItem != null) {
-                orderItem.setState(-1);
-                int update = orderItemMapper.updateByPrimaryKeySelective(orderItem);
-                if (update != 1)
-                    throw new RuntimeException("订单操作失败");
-            }
             return ajax();
         } catch (RuntimeException e) {
             log.error(e.getMessage());
